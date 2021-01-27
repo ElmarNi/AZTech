7c73e7162d5ce13a3368e5e185c138ff71c436fc
ElmarNi <46805869+ElmarNi@users.noreply.github.com>
2021-01-27 15:09:31 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 "use strict"
 $(document).ready(function () {
+    jQuery.fn.clickOutside = function(callback){
+        var $me = this;
+        $(document).mouseup(function(e) {
+            if ( !$me.is(e.target) && $me.has(e.target).length === 0 ) {
+                callback.apply($me);
+            }
+        });
+    };
     $('#slider .owl-carousel').owlCarousel({
         loop:true,
         items:1,
@@ -8,14 +16,48 @@ $(document).ready(function () {
     })
     $(window).scroll(function () { 
         if ($(window).scrollTop() > 50) {
-            $("#large-screen-menu").addClass("active")
-            $("#large-screen-menu .white-logo").addClass("d-none")
-            $("#large-screen-menu .normal-logo").removeClass("d-none")
+            $("header").addClass("active")
+            $("header .white-logo").addClass("d-none")
+            $("header .normal-logo").removeClass("d-none")
         }
         else{
-            $("#large-screen-menu").removeClass("active")
-            $("#large-screen-menu .white-logo").removeClass("d-none")
-            $("#large-screen-menu .normal-logo").addClass("d-none")
+            $("header").removeClass("active")
+            $("header .white-logo").removeClass("d-none")
+            $("header .normal-logo").addClass("d-none")
         }
     });
+    $("#small-screen-menu .open-nav").click(function (e) {
+        e.preventDefault()
+        $(".small-screen-menu-nav nav").addClass("active")
+        $("body").addClass("active")
+        $(".small-screen-overlay").addClass("active")
+    })
+    $(".small-screen-menu-nav .close-menu").click(function (e) {
+        e.preventDefault()
+        $(".small-screen-menu-nav nav").removeClass("active")
+        $("body").removeClass("active")
+        $(".small-screen-overlay").removeClass("active")
+        closeActiveSubMenu()
+    })
+    $('.small-screen-menu-nav nav').clickOutside(function(){
+        $(this).removeClass('active');
+        $("body").removeClass("active")
+        $(".small-screen-overlay").removeClass("active")
+        closeActiveSubMenu()
+    });
+    $(".small-screen-menu-nav nav .subed-li a.open-drop").click(function (e) {
+        e.preventDefault();
+        if($(this).parent().find("ul").hasClass("active")){
+            $(this).parent().find("ul").slideUp("fast").removeClass("active")
+            $(this).find("svg").removeClass("active")
+            return;
+        }
+        closeActiveSubMenu()
+        $(this).parent().find("ul").slideToggle("fast").toggleClass("active")
+        $(this).find("svg").toggleClass("active")
+    })
+    function closeActiveSubMenu () {
+        $(".small-screen-menu-nav nav .subed-li svg.active").removeClass("active")
+        $(".small-screen-menu-nav nav .subed-li ul.active").slideUp("fast").removeClass("active")
+    }
 });
